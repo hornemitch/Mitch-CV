a8783b65a3fe1cca1e76cc7b8c6b33b44162123c
hornemitch <mitchhorne92@gmail.com>
2016-06-07 11:29:20 +0200

    tbc

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,5 +1,6 @@
 require('expose?$!expose?jQuery!jquery');
 require("bootstrap-webpack");
+require("font-awesome-webpack");
 require("../styles/bootstrap-no-padding.less");
 require("../styles/main-body.less");
 require("../styles/side-bar.less");
--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,8 @@ module.exports = {
         loaders: [
         {test: /\.less$/,loader: "style!css!less"},
         {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
-        {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-        {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-        {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+        { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+
         ]
     }
 };
