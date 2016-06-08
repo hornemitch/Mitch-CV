581379ca8abf1484c901bd696edac0b8f239ac20
hornemitch <mitchhorne92@gmail.com>
2016-06-08 17:28:40 +0200

    Added more skills stuff

--- src/js/skillTabToggle.js
+++ src/js/skillTabToggle.js
@@ -2,6 +2,18 @@ let frontEndShow = $('#show-front-button');
 let frontEndHide = $('#hide-front-button');
 let frontEndSkills = $(".front-end-skills");
 
+let backEndShow = $('#show-back-button');
+let backEndHide = $('#hide-back-button');
+let backEndSkills = $(".back-end-skills");
+
+let dbShow = $('#show-db-button');
+let dbHide = $('#hide-db-button');
+let dbSkills = $(".db-skills");
+
+let testShow = $('#show-test-button');
+let testHide = $('#hide-test-button');
+let testSkills = $(".test-skills");
+
 function showButton(myButton){
     myButton.addClass("show-icon");
     myButton.removeClass("hide-icon");
@@ -29,4 +41,37 @@ frontEndHide.on('click', () =>{
     showButton(frontEndShow);
     hideButton(frontEndHide);
     hideSkills(frontEndSkills);
+});
+
+backEndShow.on('click', () =>{
+    showButton(backEndHide);
+    hideButton(backEndShow);
+    showSkills(backEndSkills);
+});
+backEndHide.on('click', () =>{
+    showButton(backEndShow);
+    hideButton(backEndHide);
+    hideSkills(backEndSkills);
+});
+
+dbShow.on('click', () =>{
+    showButton(dbHide);
+    hideButton(dbShow);
+    showSkills(dbSkills);
+});
+dbHide.on('click', () =>{
+    showButton(dbShow);
+    hideButton(dbHide);
+    hideSkills(dbSkills);
+});
+
+testShow.on('click', () =>{
+    showButton(testHide);
+    hideButton(testShow);
+    showSkills(testSkills);
+});
+testHide.on('click', () =>{
+    showButton(testShow);
+    hideButton(testHide);
+    hideSkills(testSkills);
 });
