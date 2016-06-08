492daf550109658cf1203caa974d8844f57a71df
hornemitch <mitchhorne92@gmail.com>
2016-06-08 15:59:41 +0200

    Added skills list, fixed button , did other shit too - bad commit :(

--- src/js/entry.js
+++ src/js/entry.js
@@ -7,6 +7,7 @@ require("../styles/general.less");
 require("../styles/main-body.less");
 require("../styles/side-bar.less");
 require("../styles/contact-div.less");
+require("../styles/skills-div.less");
 
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip({placement: "right", trigger: "hover"});
